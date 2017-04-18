d92b2e451aafefcc39d7cf88dfaea3e90eac9bf2
Ariel Fisher <fishondor@gmail.com>
2017-04-18 22:49:27 +0300

    Added prefixer to static css loader

--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,7 @@ module.exports = {
         use: ExtractTextPlugin.extract({
           fallback: 'style-loader',
           //resolve-url-loader may be chained before sass-loader if necessary
-          use: ['css-loader', 'sass-loader']
+          use: ['css-loader', 'sass-loader', 'postcss-loader']
         })
       }
    ]
